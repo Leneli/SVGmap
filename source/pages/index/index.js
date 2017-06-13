6b8721d2d3e9902fdb625b943758693154e7320c
Elena <leneli.lehmann@yahoo.de>
2017-06-13 11:50:05 +0300

    Create index.js
    Удалены остатки старого кода

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,14 +1,7 @@
 import "./index.scss";
 import "normalize.css";
 
-//import createMenu from "../../components/menu/menu";
-//var menu = createMenu(["Главная", "Обо мне", "Портфолио", "Контакты"], "menu");
-//document.body.appendChild(menu);
-
-
 import coordinates from "../../components/polygons/world-polygons";
 import createMap from "../../components/map/map";
 
 createMap(coordinates());
-
-console.log(coordinates());
